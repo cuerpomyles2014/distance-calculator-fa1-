d875da374d370fb97afa549a96652a6c6294367c
Goldenbergel <mcmcuerpo31@src.pshs.edu.ph>
2026-08-13 14:07:14 +0800

    Updated code to have micro notes inside (explains code)

--- distance_calculator.py
+++ distance_calculator.py
@@ -1,11 +1,22 @@
+# Makes it so that it can import math related code (e.g. sqrt) which makes it able to use it.
 import math
+
+# Asks the user for the first x coordinate.
 x1 = float(input("Enter the first x coordinate: "))
+
+# Asks the user for the first y coordinate.
 y1 = float(input("Enter the first y coordinate: "))
+
+# Asks the user for the second x coordinate.
 x2 = float(input("Enter the second x coordinate: "))
+
+# Asks the user for the second y coordinate.
 y2 = float(input("Enter the second y coordinate: "))
 
+# Calculates the distance using the imported math code and the coordinates given. 
 distance = math.sqrt (pow(x2 - x1, 2) + pow(y2 - y1, 2))
 
+# Prints the calculated distance
 print ("The distance between the two points is:", f"{distance:.2f}")
 
 
